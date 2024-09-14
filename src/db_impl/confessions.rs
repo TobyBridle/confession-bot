385a29cb3010bca45f782be92138a03792d771b9
Toby Bridle <foreignspaghettibolognese@gmail.com>
2024-09-14 12:39:01 +0100

    feat(confession): implement `get_confession_by_id`

--- src/db_impl/confessions.rs
+++ src/db_impl/confessions.rs
@@ -1,36 +1,33 @@
-use std::{
-    error::Error,
-    ops::{Deref, DerefMut},
-};
+use std::error::Error;
 
-use diesel::{
-    associations::HasTable, ExpressionMethods, JoinOnDsl, QueryDsl, RunQueryDsl, SelectableHelper,
-    SqliteConnection,
-};
+use confession_bot_rs::establish_connection;
+use diesel::{BoolExpressionMethods, ExpressionMethods, QueryDsl, RunQueryDsl, SelectableHelper};
 
 use crate::{
     models::Confession,
-    schema::{confession, guild},
+    schema::{
+        confession,
+        guild::{self},
+    },
 };
 
 pub async fn get_confession_by_id(
-    connection: &mut SqliteConnection,
-    confession_id: String,
-    guild_id: String,
+    db_url: String,
+    message_id: String,
+    _guild_id: String,
 ) -> Result<Confession, Box<dyn Error>> {
-    use self::confession::dsl::*;
-    match confession
-        .inner_join(guild::table)
+    let mut connection = establish_connection(db_url);
+    match guild::table
+        .inner_join(confession::table)
+        .filter(
+            confession::message_id
+                .eq(message_id)
+                .and(guild::guild_id.eq(_guild_id)),
+        )
         .select(Confession::as_select())
-        .load::<Confession>(connection)
+        .get_result(&mut connection)
     {
-        Ok(c) => {
-            return if let Some(c) = c.first() {
-                Ok(c.clone())
-            } else {
-                Err("Test".into())
-            }
-        }
+        Ok(c) => return Ok(c.clone()),
         Err(e) => return Err(Box::new(e)),
     }
 }
