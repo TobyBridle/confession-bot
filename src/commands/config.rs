fbdc3dba4cd9d5d8b2d55c988d48b39f95a54e64
fabseman <personal@fabseman.space>
2024-09-21 11:39:59 +0900

    oof, missed a few unwraps

--- src/commands/config.rs
+++ src/commands/config.rs
@@ -35,80 +35,71 @@ pub async fn config_guild(
         if let Some(guild) = guilds::get_guild(config.db_url.clone(), guild_id.to_string()).await? {
             let mut changelog = String::new();
             let mut guild_config: GuildConfig = serde_json::from_str(guild.config.as_str())?;
-            if delete_vote_min.is_some() {
+            if let Some(delete_vote_min_res) = delete_vote_min {
                 changelog.push_str(
                     format!(
                         "Delete Vote Min: {} -> {}\n",
-                        guild_config.delete_vote_min,
-                        delete_vote_min.unwrap()
+                        guild_config.delete_vote_min, delete_vote_min_res
                     )
                     .as_str(),
                 );
-                guild_config.delete_vote_min = delete_vote_min.unwrap();
+                guild_config.delete_vote_min = delete_vote_min_res;
             }
-            if expose_vote_min.is_some() {
+            if let Some(expose_vote_min_res) = expose_vote_min {
                 changelog.push_str(
                     format!(
                         "Expose Vote Min: {} :arrow_right: {}\n",
-                        guild_config.expose_vote_min,
-                        expose_vote_min.unwrap()
+                        guild_config.expose_vote_min, expose_vote_min_res
                     )
                     .as_str(),
                 );
-                guild_config.expose_vote_min = expose_vote_min.unwrap();
+                guild_config.expose_vote_min = expose_vote_min_res;
             }
-            if expose_vote_role.is_some() {
+            if let Some(expose_vote_role_res) = expose_vote_role {
                 changelog.push_str(
                     format!(
                         "Expose Vote Role: {} :arrow_right: {}\n",
-                        guild_config
-                            .expose_vote_role
-                            .clone()
-                            .unwrap_or("Unset".to_owned()),
-                        expose_vote_role.unwrap()
+                        guild_config.expose_vote_role.unwrap_or("Unset".to_owned()),
+                        expose_vote_role_res
                     )
                     .as_str(),
                 );
-                guild_config.expose_vote_role = Some(expose_vote_role.unwrap().to_string());
+                guild_config.expose_vote_role = Some(expose_vote_role_res.to_string());
             }
-            if role_ping.is_some() {
+            if let Some(role_ping_res) = role_ping {
                 changelog.push_str(
                     format!(
                         "Role Ping: {} :arrow_right: {}\n",
-                        guild_config
-                            .role_ping
-                            .clone()
-                            .unwrap_or(String::from("Unset")),
-                        role_ping.unwrap()
+                        guild_config.role_ping.unwrap_or("Unset".to_string()),
+                        role_ping_res
                     )
                     .as_str(),
                 );
-                guild_config.role_ping = Some(role_ping.unwrap().to_string());
+                guild_config.role_ping = Some(role_ping_res.to_string());
             }
 
-            guilds::update_guild(
-                config.db_url.clone(),
-                ctx.guild_id().unwrap().to_string(),
-                if channel_id.is_some() {
-                    // TODO: Check if channel is text based
-                    changelog.push_str(
-                        format!(
-                            "Confession Channel ID: {} :arrow_right: {}\n",
-                            guild
-                                .confession_channel_id
-                                .clone()
-                                .unwrap_or(String::from("Unset")),
-                            channel_id.unwrap()
-                        )
-                        .as_str(),
-                    );
-                    Some(channel_id.unwrap().to_string())
-                } else {
-                    guild.confession_channel_id
-                },
-                guild_config,
-            )
-            .await?;
+            if let Some(guild_id) = ctx.guild_id() {
+                guilds::update_guild(
+                    config.db_url.clone(),
+                    guild_id.to_string(),
+                    if let Some(channel_id_res) = channel_id {
+                        // TODO: Check if channel is text based
+                        changelog.push_str(
+                            format!(
+                                "Confession Channel ID: {} :arrow_right: {}\n",
+                                guild.confession_channel_id.unwrap_or("Unset".to_owned()),
+                                channel_id_res
+                            )
+                            .as_str(),
+                        );
+                        Some(channel_id_res.to_string())
+                    } else {
+                        guild.confession_channel_id
+                    },
+                    guild_config,
+                )
+                .await?;
+            }
             ctx.send(
                 CreateReply::default().embed(
                     CreateEmbed::default()
