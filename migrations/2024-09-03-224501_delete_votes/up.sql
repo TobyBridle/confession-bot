840b76daaf3b7bc37999f55b1f897d4f87b9f4be
Toby Bridle <foreignspaghettibolognese@gmail.com>
2024-09-27 01:51:40 +0100

    fix: change sql vote constraint names

--- migrations/2024-09-03-224501_delete_votes/up.sql
+++ migrations/2024-09-03-224501_delete_votes/up.sql
@@ -2,7 +2,7 @@ CREATE TABLE `delete_votes` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`confession_id` integer NOT NULL,
 	`author_id` integer NOT NULL,
-	`vote_type` text CHECK (`vote_type` IN ("DELETE", "EXPOSE") ) NOT NULL DEFAULT "DELETE",
+	`vote_type` text CHECK (`vote_type` IN ("delete_vote", "expose_vote") ) NOT NULL DEFAULT "delete_vote",
 	`timestamp` timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL,
 	FOREIGN KEY (`confession_id`) REFERENCES `confession`(`id`) ON UPDATE no action ON DELETE no action,
 	FOREIGN KEY (`author_id`) REFERENCES `authors`(`id`) ON UPDATE no action ON DELETE no action
