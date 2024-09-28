7dc9afea2719c16560d549c98fa0142739cd9d31
Toby Bridle <foreignspaghettibolognese@gmail.com>
2024-09-29 00:38:36 +0100

    feat: add delete component implementation

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,13 +1,23 @@
+use std::{str::FromStr, sync::Arc};
+
+use confession_bot_rs::{VoteType, DELETE_VOTE_STR};
 use poise::{
     builtins,
-    serenity_prelude::{self as serenity, CreateEmbed, CreateMessage},
+    serenity_prelude::{
+        self as serenity, ActionRowComponent, ButtonStyle, CreateActionRow, CreateButton,
+        CreateEmbed, CreateMessage, EditMessage, ReactionType,
+    },
     FrameworkContext, FrameworkError,
 };
 use serenity::FullEvent;
 use tokio::sync::RwLock;
 use tracing::{error, info};
 
-use crate::{db_impl::guilds, models::GuildConfig, Config};
+use crate::{
+    db_impl::{guilds, votes::update_delete_vote},
+    models::GuildConfig,
+    Config,
+};
 
 pub mod confess;
 pub mod config;
@@ -25,10 +35,77 @@ pub async fn event_handler(
     match &event {
         FullEvent::InteractionCreate { interaction } => {
             if let Some(cmp) = interaction.as_message_component() {
-                println!(
-                    "We got this interaction (we dont handle it): {:?}",
-                    cmp.data.custom_id
-                );
+                let reaction_type: VoteType = cmp.data.custom_id.to_string().into();
+                let message_id = cmp.message.id.to_string();
+                let author_id = cmp.user.id.to_string();
+                let guild_id = cmp
+                    .guild_id
+                    .ok_or("Could not get Guild ID for interaction.");
+                if let Err(e) = guild_id {
+                    return Err(Box::from(e.to_string()));
+                }
+                let guild_id = guild_id.unwrap().to_string();
+
+                let data: Arc<Data> = framework.serenity_context.data();
+                let config = data.config.read().await;
+
+                if reaction_type == VoteType::DELETE {
+                    let updated =
+                        update_delete_vote(config.db_url.clone(), author_id, message_id, guild_id)
+                            .await?;
+
+                    let updated_message = if updated.0 == updated.1 {
+                        EditMessage::new()
+                            .embed(
+                                CreateEmbed::new()
+                                    .title(
+                                        cmp.message
+                                            .embeds
+                                            .first()
+                                            .unwrap()
+                                            .title
+                                            .as_ref()
+                                            .unwrap()
+                                            .to_string(),
+                                    )
+                                    .description(format!(
+                                        "Deleted Confession ({} votes)",
+                                        updated.0
+                                    ))
+                                    .color(0xFF0000),
+                            )
+                            .components(vec![])
+                    } else {
+                        let action_row = match cmp.message.components.first() {
+                            Some(c) => c,
+                            None => {
+                                return Err(Box::from("Could not get components from message."))
+                            }
+                        };
+
+                        let expose = match action_row.components.iter().nth(1) {
+                            Some(ActionRowComponent::Button(b)) => b,
+                            e => {
+                                panic!("Did not find the right component! Got: {:?}", e)
+                            }
+                        };
+                        let components = CreateActionRow::Buttons(vec![
+                            CreateButton::new(DELETE_VOTE_STR)
+                                .emoji(ReactionType::from_str("🗑")?)
+                                .style(ButtonStyle::Danger)
+                                .label(format!("Delete ({}/{})", updated.0, updated.1)),
+                            expose.clone().into(),
+                        ]);
+                        EditMessage::new().components(vec![components])
+                    };
+
+                    cmp.message
+                        .clone()
+                        .edit(&framework.serenity_context.http, updated_message)
+                        .await?;
+                } else {
+                    panic!("Not Implemented Reaction type for Expose")
+                }
             }
         }
         FullEvent::Ready { data_about_bot } => {
@@ -78,9 +155,9 @@ pub async fn event_handler(
                             .description("It is recommended that you use a designated confession channel in order to prevent 'spam' of an already used text-channel.")
                             .fields(
                                 vec![
-                                    ("Confession Channel", 
+                                    ("Confession Channel",
                                     if let Some(confession_channel) = guild.confession_channel_id {
-                                        format!("<#{}>", confession_channel) 
+                                        format!("<#{}>", confession_channel)
                                     } else {
                                         "Unset".to_owned()
                                     }, true),
@@ -89,18 +166,18 @@ pub async fn event_handler(
                                     ("Minimum Vote (Delete)", config.delete_vote_min.to_string(), true),
                                     ("", "".to_owned(), true),
                                     ("Minimum Vote (Expose)", config.expose_vote_min.to_string(), true),
-                                    ("Minimum Role to register  (Expose)", 
+                                    ("Minimum Role to register  (Expose)",
                                     if let Some(expose_vote_role) = config.expose_vote_role {
                                         format!("<@{}>", expose_vote_role)
                                     } else {
-                                        "Unset".to_string() 
+                                        "Unset".to_string()
                                     }, true),
                                     ("", "".to_owned(), true),
-                                    ("Role Ping", 
+                                    ("Role Ping",
                                     if let Some(ping_role) = config.role_ping {
                                         format!("<@{}>", ping_role)
                                     } else {
-                                        "Unset".to_owned() 
+                                        "Unset".to_owned()
                                     }, true)
                                 ]
                             )
