150977a2c62f27004cee1922c6069a162f2d7804
Toby Bridle <foreignspaghettibolognese@gmail.com>
2024-09-29 00:05:44 +0100

    feat: add vote_type enum & conversions

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,30 @@ pub fn establish_connection(db_url: String) -> SqliteConnection {
     SqliteConnection::establish(&db_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", db_url))
 }
+
+#[derive(PartialEq, Eq)]
+pub enum VoteType {
+    DELETE,
+    EXPOSE,
+}
+
+pub const DELETE_VOTE_STR: &str = "delete_vote";
+pub const EXPOSE_VOTE_STR: &str = "expose_vote";
+impl Into<String> for VoteType {
+    fn into(self) -> String {
+        match self {
+            VoteType::DELETE => DELETE_VOTE_STR.to_string(),
+            VoteType::EXPOSE => EXPOSE_VOTE_STR.to_string(),
+        }
+    }
+}
+
+impl Into<VoteType> for String {
+    fn into(self) -> VoteType {
+        return match self.to_lowercase().as_str() {
+            DELETE_VOTE_STR => VoteType::DELETE,
+            EXPOSE_VOTE_STR => VoteType::EXPOSE,
+            _ => panic!("Could not convert {} into a VoteType.", self),
+        };
+    }
+}
