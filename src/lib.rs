733a4a19b709752d5b3d117c401afae75c9b310e
Toby Bridle <foreignspaghettibolognese@gmail.com>
2024-09-29 21:08:42 +0100

    feat: add `Clone` and `Copy` traits to `VoteType`

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ pub fn establish_connection(db_url: String) -> SqliteConnection {
         .unwrap_or_else(|_| panic!("Error connecting to {}", db_url))
 }
 
-#[derive(PartialEq, Eq)]
+#[derive(PartialEq, Eq, Clone, Copy)]
 pub enum VoteType {
     DELETE,
     EXPOSE,
